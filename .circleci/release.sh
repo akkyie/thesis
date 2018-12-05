ad7b882e35a62072e88d30b483826491cbe4be5a
Akio Yasui <akkyie01@gmail.com>
2018-12-06 07:32:14 +0900

    タグのつけ方を再変更

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -2,21 +2,12 @@
 
 set -eux
 
-git config --global user.email "aky@keio.jp"
-git config --global user.name "Akio Yasui (Circle CI)"
-
 PDF_FILE="build/thesis.pdf"
-REDPEN_FILES="build/redpen.*.xml"
+TAG=$(date +'%Y.%m.%d-%H.%M.%S')
 DATE=$(date +'%Y/%m/%d %H:%M:%S')
 
-if [ $CIRCLE_BRANCH = "master" ]; then
-  TAG=$(date +'%Y.%m.%d-%H%M%S')
-else
-  TAG="$(date +'%Y.%m.%d')-$CIRCLE_BRANCH"
-fi
-
-git tag -f $TAG
-git push -f --tags
+git tag $TAG
+git push --tags
 echo "New tag: $TAG"
 
 echo "Cleaning up under build/"
